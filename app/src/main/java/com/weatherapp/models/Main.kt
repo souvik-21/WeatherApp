0fbfd087d16887127da09c09f44348c0f7befbfb
Souvik Kar <karsouvik7872115553@gmail.com>
2021-09-14 07:45:49 +0530

    Initial Commit

---
+++ app/src/main/java/com/weatherapp/models/Main.kt
@@ -0,0 +1,11 @@
+package com.weatherapp.models
+
+import java.io.Serializable
+
+data class Main(
+    val temp: Double,
+    val pressure: Double,
+    val humidity: Int,
+    val tempMin: Double,
+    val tempMax: Double
+) : Serializable
